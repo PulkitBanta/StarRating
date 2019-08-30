1c0271d0915ffd4f8e0b2d610b1c5ec998ab0bdd
Pulkit <Pulkitbanta55555@gmail.com>
2019-08-30 11:22:34 +0530

    Repaired the code

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // Add this code to your existing JS file
+$(document).ready(function (){
+    // setting the stars color on clicking the stars
+    $("span").click(function() {
+        $(this).prevAll().css('color', 'orange'), $(this).css('color', 'orange');
+        $(this).nextAll().css('color', 'black');
+    });
 
-        // setting the stars color on clicking the stars
-        $("span").click(function() {
-            $(this).prevAll().css('color', 'orange'), $(this).css('color', 'orange');
-            $(this).nextAll().css('color', 'black');
+    // to color the stars on hovering over them
+    $('span').mouseover(function() {
+        $(this).prevAll().addClass('checked'), $(this).addClass('checked');
+        $(this).mouseleave(function() {
+            $('span').removeClass('checked');
         });
-
-        // to color the stars on hovering over them
-        $('span').mouseover(function() {
-            $(this).prevAll().addClass('checked'), $(this).addClass('checked');
-            $(this).mouseleave(function() {
-                $('span').removeClass('checked');
-            });
-        });
+    });
+})
